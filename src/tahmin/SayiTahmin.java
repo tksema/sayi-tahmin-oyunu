3124d79ec0578092871d78289af4974bd9042720
sema <tksema50@gmail.com>
2023-01-03 11:14:10 +0300

    first commit

--- src/tahmin/SayiTahmin.java
+++ src/tahmin/SayiTahmin.java
@@ -2,6 +2,35 @@ package tahmin;
 
 public class SayiTahmin {
     public static void main(String[] args) {
-        
+             /*
+   1 ile 100 arasında bir sayı belirleyelim.
+   (bilgisayar dışında bir toplulukta, bulunanlara tek tek sorulur, bilen kaybeder)
+   kullanıcıdan o sayıyı bilmemesi gerektiğini söyleyelim.
+   bilirse oyunu kaybedeceği söylenir.
+   istenen sayılar dışından söylerse de oyunu kaybeder.
+   örnek: tutulan sayı 62 olsun.
+   kullanıcıdan sayı istenir.
+   - 1 ile 100 arasında bir sayı söyleyin.
+   - 20
+   - 20 ile 100 arasında
+   - 30
+   - 30 ile 100 arasında
+   - 80
+   - 30 ile 80 arasında
+   - 50
+   - 50 ile 80 arasında
+   - 60
+   - 60 ile 80 arasında
+   - 70
+   - 60 ile 70 arasında
+   - 65
+   - 60 ile 65 arasında
+   - 63
+   - 60 ile 63 arasında
+   - 61
+   - 61 ile 63 arasında
+   - 62
+   -BİLDİNİZ....
+    */
     }
 }
